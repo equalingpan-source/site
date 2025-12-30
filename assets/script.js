956cd44127c2577b4bd571f50d68473b429c5aa2
equalingpan-source <equaling.pan@gmail.com>
2025-12-30 19:20:00 +0900

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,11 @@
-/* script.js */
+const SITE_CONFIG = {
+  // Googleフォーム / Tally / Formspree のURLに置き換え
+  contactFormUrl: "https://example.com"
+};
+
+(function init() {
+  const links = document.querySelectorAll("#contactLink");
+  links.forEach((a) => {
+    a.href = SITE_CONFIG.contactFormUrl;
+  });
+})();
